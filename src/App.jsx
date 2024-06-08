cec026b2ef9caea5ea5ab113f070766e855041a8
dexter3k <schooldev3000@gmail.com>
2024-06-09 01:26:10 +0300

    Added tailwin

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p>Count is {count}</p>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          +
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          -
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello, world!
+      </h1>
     </>
   )
 }
